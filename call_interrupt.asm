10d4017e1efce5fb931c2eb150c75c294956fbd4
nannan <nannan@localhost.localdomain>
2018-11-03 21:39:39 -0400

    added a sample with irq = 4

--- call_interrupt.asm
+++ call_interrupt.asm
@@ -1,12 +1,20 @@
 ;global asm_call_interrupt
-section .data
+;section .data
 ;asm_call_interrupt:
-msg	db	"asm_call_interrupt is calling"
-section .text
-	global	_start
-_start:
-	int 0x2F
-	iret
+;msg	db	"asm_call_interrupt is calling"
+;section .text
+;	global	_start
+;_start:
+;	int 0x2F
+;	iret
 ;msg	db	"asm_call_interrupt is called"
 ;section .data
 ;message: db    "asm_call_interrupt is called", 10
+
+global asm_call_interrupt
+asm_call_interrupt:
+	mov ah, 0x0e
+	mov al, '!'
+	int 0x04
+;	iret
+msg	db	"asm_call_interrupt is called!!!", 10

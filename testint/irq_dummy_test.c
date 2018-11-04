9391488374280174e337768ef8612cb3196cd612
nnzhaocs <znannan1@vt.edu>
2018-11-04 10:03:47 -0500

    testing int

---
+++ testint/irq_dummy_test.c
@@ -0,0 +1,87 @@
+/*
+ * kcmask.c - kcmask driver file
+ *
+ * kcmask is a backend for frontswap that takes pages that are in the process
+ * of being swapped out and attempts to compress and store them in a
+ * RAM-based memory pool.  This can result in a significant I/O reduction on
+ * the swap device and, in the case where decompressing from RAM is faster
+ * than reading from the swap device, can also improve workload performance.
+ *
+ * Copyright (C) 2012  Seth Jennings <sjenning@linux.vnet.ibm.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/cpu.h>
+#include <linux/highmem.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
+#include <linux/atomic.h>
+#include <linux/frontswap.h>
+#include <linux/rbtree.h>
+#include <linux/swap.h>
+#include <linux/crypto.h>
+#include <linux/mempool.h>
+#include <linux/zpool.h>
+
+#include <linux/mm_types.h>
+#include <linux/page-flags.h>
+#include <linux/swapops.h>
+#include <linux/writeback.h>
+#include <linux/pagemap.h>
+//NANNAN
+#include <linux/unistd.h>
+#include <linux/mm.h>
+#include <asm/unistd.h>
+#include <linux/bio.h>
+#include <linux/kernel_stat.h>
+#include <linux/gfp.h>
+#include <linux/buffer_head.h>
+#include <linux/blkdev.h>
+#include <linux/uio.h>
+#include <asm/pgtable.h>
+
+#include <linux/init.h>
+#include <linux/irq.h>
+#include <linux/irqdomain.h>
+#include <linux/interrupt.h>
+
+
+extern void call_interrupt(void);
+
+static int __init init_dummy_irq_test(void)
+{
+    printk("initing test dummy irq.\n");
+    
+//    call_interrupt();
+    {
+        __asm__ __volatile__ ("int $58");
+    }
+
+    return 0;
+}
+static void __exit exit_dummy_irq_test(void)
+{
+    printk("exit test dummy irq\n");
+    return ;
+}
+
+
+module_init(init_dummy_irq_test);
+module_exit(exit_dummy_irq_test);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nannan Zhao <znannan1@vt.edu>");
+MODULE_DESCRIPTION("Kernel support for CMASK");
